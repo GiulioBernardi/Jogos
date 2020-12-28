f115446eace3bf25a5f977cff5d603a67425c2cf
Giulio Bernardi <giulioccbernardi@gmail.com>
2020-12-28 08:45:05 -0300

    Arqv triangulos criado. Mensagens de boas vindas adicionadas

---
+++ triangulos.py
@@ -0,0 +1,3 @@
+print("Bem-vindo")
+print("Esse script irá te auxiliar em exercícios com triângulos")
+print("Basta forncer as suas medidas, que você receberá de volta se a classificação do triângulo!")
