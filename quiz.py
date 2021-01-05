6d1adb5a4fbedd6ecf0f5b4bf118fd497d172d11
Giulio Bernardi <giulioccbernardi@gmail.com>
2021-01-05 12:01:29 -0300

    Input do usuário para escolha de tema criada

---
+++ quiz.py
@@ -0,0 +1,10 @@
+
+def jogar():
+    print("Bem-vindo ao meu QUIZ")
+    print("Escolha uma opção de tema\n(1)Geografia\n(2)História\n(3)Cinema\n(4)Esportes\n(5)Tecnologia\n(6)Biologia\n(7)Matemática\n(8)Física\n(9)Todos\n(10)Aleatório")
+    tema = int(input("Escolha o tema: "))
+
+
+    if(__name__ == "__main__"):
+        jogar()
+
