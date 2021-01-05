ebfc669b66ea1a036fefedbd356fa8655ebccaf2
Giulio Bernardi <giulioccbernardi@gmail.com>
2021-01-05 12:14:32 -0300

    Estrutura básica de decisão criada para cada a escolha do usuário

--- quiz.py
+++ quiz.py
@@ -1,10 +1,42 @@
-
 def jogar():
     print("Bem-vindo ao meu QUIZ")
     print("Escolha uma opção de tema\n(1)Geografia\n(2)História\n(3)Cinema\n(4)Esportes\n(5)Tecnologia\n(6)Biologia\n(7)Matemática\n(8)Física\n(9)Todos\n(10)Aleatório")
     tema = int(input("Escolha o tema: "))
 
+    geografia = tema == 1
+    historia = tema == 2
+    cinema = tema == 3
+    esportes = tema == 4
+    tecnologia = tema == 5
+    biologia = tema == 6
+    matematica = tema == 7
+    fisica = tema == 8
+    todos = tema == 9
+    aleatorio = tema == 10
 
-    if(__name__ == "__main__"):
-        jogar()
+    if (geografia):
+        pass
+    elif (historia):
+        pass
+    elif (cinema):
+        pass
+    elif (esportes):
+        pass
+    elif (tecnologia):
+        pass
+    elif (biologia):
+        pass
+    elif (matematica):
+        pass
+    elif (fisica):
+        pass
+    elif (todos):
+        pass
+    elif (aleatorio):
+        pass
+    else:
+        print("Valor inválido!")
+
+if(__name__ == "__main__"):
+    jogar()
 
